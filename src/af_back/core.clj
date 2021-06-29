a2e7b23a6ca56c01ca8ed86c418dd20fe641cb01
Thomas SEGURA <thomassegura@gmail.com>
2021-06-29 13:34:21 +0200

    app public homepage

--- src/af_back/core.clj
+++ src/af_back/core.clj
@@ -4,8 +4,8 @@
             [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer :all]
-            [clojure.data.json :as json]
-            [jumblerg.middleware.cors :refer [wrap-cors]])
+            [ring.util.response :refer :all]
+            [clojure.data.json :as json])
   (:gen-class))
 
 (defn scrapping-result [page]
@@ -14,15 +14,16 @@
    :body    (json/write-str (current-annonces (Integer/parseInt page)))})
 
 (defroutes app-routes
-  (GET "/:page" [page] (scrapping-result page))
+  (GET "/" [] (slurp "resources/public/home.html"))
+  (GET "/scrap/:page" [page] (scrapping-result page))
   (route/not-found "Error, page not found!"))
 
 (defn -main
   "This is our main entry point"
   [& args]
-  (let [port (Integer/parseInt (or (System/getenv "PORT") "3000"))]
+  (let [port (Integer/parseInt (or (System/getenv "PORT") "8080"))]
     ; Run the server with Ring.defaults middleware
-    (server/run-server (wrap-cors (wrap-defaults #'app-routes site-defaults) #".*")
+    (server/run-server (wrap-defaults #'app-routes site-defaults)
                        {:port port})
     ; Run the server without ring defaults
     ;(server/run-server #'app-routes {:port port})
