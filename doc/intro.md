6f50dc297fcdee963d0f5b5188f3b31cb8ef41d5
Thomas SEGURA <thomassegura@gmail.com>
2021-07-14 18:53:27 +0200

    lein plugin

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to af-back
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.11.0-alpha1"]
                  [clj-http "3.12.0"]
                  [hickory "0.7.1"]
                  [compojure "1.6.2"]
@@ -12,6 +12,16 @@
                  [org.clojure/data.json "0.2.6"]
                  [http-kit "2.3.0"]
                  [jumblerg/ring-cors "2.0.0"]]
+  :plugins [[io.taylorwood/lein-native-image "0.3.1"]]
+  :native-image {:name "af-scrap"
+                 :graal-bin "/Library/Java/JavaVirtualMachines/graalvm-ce-java11-21.1.0/Contents/Home/bin"
+                 :jvm-opts ["-Dclojure.compiler.direct-linking=true"]
+                 :opts ["-H:EnableURLProtocols=http"
+                        "--report-unsupported-elements-at-runtime" ;; ignore native-image build errors
+                        "--initialize-at-build-time"
+                        "--no-server" ;; TODO issue with subsequent builds failing on same server
+                        "--verbose"]}
   :main af-back.core
   :uberjar-name "af-back-standalone.jar"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+)
